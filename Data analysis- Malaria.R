e4481f0ba193836b67c693130307b4234aa87ce1
Clarkyyy03 <134625159+Clarkyyy03@users.noreply.github.com>
2024-10-20 10:44:20 +0100

    Data analysis- Malaria.R

--- Data analysis- Malaria.R
+++ Data analysis- Malaria.R
@@ -2,18 +2,31 @@ install.packages("pheatmap")
 install.packages("data.table")
 install.packages("readxl")
 install.packages("ggvenn")
+install.packages("dplyr")
+install.packages("tidyr")
 
 library(ggvenn)
 library(readxl)
 library(data.table)
 library(tidyverse)
 library(pheatmap)
+library(dplyr)
+library(tidyr)
+
 
 #Venn diagrams
 d1 <- read_excel("~/Desktop/data analysis/ATII 24-25 RNA-Seq.xlsx")
  list = list('7dvs.24h.DOWN' = d1$`Gene ID`[d1$Regulation.7vs24== "DOWN"],
              '7dvs.1h.DOWN' = d1$`Gene ID`[d1$Regulation.7vs1== "DOWN"])
 ggvenn(list) 
+
+# dl1 = list(`24vs1.UP`=na.omit(d$`Gene ID`[d$`log2ratio:24h v 1h` > 1 & d$`p-val:24h v 1h` < 0.05]),
+#            `24vs1.DOWN`=na.omit(d$`Gene ID`[d$`log2ratio:24h v 1h` < -1 & d$`p-val:24h v 1h` < 0.05]),
+#            `7vs24.UP`=na.omit(d$`Gene ID`[d$`log2ratio:7d v 24h`>1 & d$`p-val:7d v 24h`<0.05]),
+#            `7vs24h.DOWN`=na.omit(d$`Gene ID`[d$`log2ratio:7d v 24h`< -1 & d$`p-val:7d v 24h`<0.05]),
+#            `7vs1.UP`=na.omit(d$`Gene ID`[d$`log2ratio:7d v 1h`> 1 & d$`p-val:7d v 1h`<0.05]),
+#            `7vs1.DOWN`=na.omit(d$`Gene ID`[d$`log2ratio:7d v 1h`< -1 & d$`p-val:7d v 1h`<0.05]) )
+# summary(dl1)
 
 #List those overlapped genes
 library(ggvenn)
@@ -23,21 +36,4 @@ gene_sets <- list(
 )
 overlap <- Reduce(intersect, gene_sets)
 write.table(overlap, file = "overlapping_genes.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
-
 #Further GO enrichment analysis
-getwd()
-go.enrich = read.csv("/Users/clarkeyin/Desktop/data analysis/P.falciparum/7h.24,1h down.tsv",  sep = "\t", header = TRUE)
-
-ggplot(go.enrich, aes(x = -log10(adjusted_p_value), y = term_name, size = interaction_size, color = highlighted)) +
-  geom_point() +
-  theme_bw() +
-  labs(
-    title = "GO Enrichment Analysis",
-    x = "-log10(Adjusted P-Value)",
-    y = "GO Terms",
-    size = "Interaction Size",
-    color = "Highlighted"
-  )
-a = go.enrich$
-tmp = go.enrich %>% filter(hightlighted== TRUE) %>% separate_rows("interactions", seq = ",")
-genelist = unique()
